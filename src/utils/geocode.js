c5638a1cb7d235c4e80df02cf3aa15b88d29172b
Erin Coralic <coralic.erin@gmail.com>
2022-10-24 23:36:02 +0200

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZXJpeDk5OSIsImEiOiJjbDAyOGdwb2oxMXBpM2p0MWJ4NDl6eXY1In0._zPDj-TfbLXk2yNv09TEXw&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+
+            })
+        }
+    })    
+}
+
+module.exports = geocode
